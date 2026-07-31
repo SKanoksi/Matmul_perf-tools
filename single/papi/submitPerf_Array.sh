06ae467489e948cf9034d7a99629a32ff3f64b8c
Somrath Kanoksirirath <somrathk@gmail.com>
2026-07-31 15:21:05 +0700

    Add files via upload

--- single/papi/submitPerf_Array.sh
+++ single/papi/submitPerf_Array.sh
@@ -3,26 +3,34 @@
 #SBATCH -N 1                   # Number of node
 #SBATCH --ntasks-per-node=1    # Number of MPI processes per node
 #SBATCH --cpus-per-task=1      # Number of OpenMP threads per MPI process
-#SBATCH -t 00:10:00            # Job runtime limit
+#SBATCH -t 00:30:00            # Job runtime limit
 #SBATCH -J PAPI                # Job name
-#SBATCH -A thaisc              # Slurm Account 
-#SBATCH --reservation=         # Reservation, if any
+#SBATCH -A thaisc              # Slurm Account
+#SBATCH --reservation=         # Reservation, if any 
+#SBATCH --array=0-10           # Job array -> differnt SLURM_ARRAY_TASK_ID
 
 module reset
 module swap PrgEnv-cray PrgEnv-gnu
 module load perftools
 
-CC -DUSE_ALGOR=0 -O3 -o ./matmul.exe -I../../src ../../src/matmul_main.cpp 
+NUM_REPEAT=20
+MATRIX_SIZE=$((250+${SLURM_ARRAY_TASK_ID}))
+
+mkdir ${MATRIX_SIZE}
+cd ${MATRIX_SIZE}
+
+CC -DUSE_ALGOR=0 -O3 -o ./matmul.exe \
+	-I../../../src ../../../src/matmul_main.cpp 
 
 pat_build -o ./matmul_pat.exe ./matmul.exe
 
 # ---
-#
+
 export PAT_RT_PERFCTR="PAPI_TOT_CYC,PAPI_TOT_INS,PAPI_VEC_INS"
 export PAT_RT_PERFCTR="${PAT_RT_PERFCTR},PAPI_FP_INS,PAPI_FP_OPS"
 export PAT_RT_PERFCTR="${PAT_RT_PERFCTR},PAPI_BR_INS,PAPI_BR_MSP"
 
-srun ./matmul_pat.exe 20 1000 1000 1000
+srun ./matmul_pat.exe ${NUM_REPEAT} ${MATRIX_SIZE} ${MATRIX_SIZE} ${MATRIX_SIZE}
 
 pat_report ./matmul_pat.exe+*-* >& pat_instr.out
 
@@ -33,7 +41,7 @@ rm -rf ./matmul_pat.exe+*-*
 export PAT_RT_PERFCTR="PAPI_L1_DCA,PAPI_L1_DCM,PAPI_L2_DCM"
 export PAT_RT_PERFCTR="${PAT_RT_PERFCTR},PAPI_TOT_INS,PAPI_TLB_DM"
 
-srun ./matmul_pat.exe 20 1000 1000 1000
+srun ./matmul_pat.exe ${NUM_REPEAT} ${MATRIX_SIZE} ${MATRIX_SIZE} ${MATRIX_SIZE}
 
 pat_report ./matmul_pat.exe+*-* >& pat_cache.out
 
